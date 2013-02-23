c973f2b6270a8e96c4d88633c1a2635f98a28715
Eder Roger <eder.roger.souza@gmail.com>
2013-02-23 08:39:21 -0300

    add other branch

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  attr_accessible :email, :name
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+  
+end
